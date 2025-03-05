082f532d1381c2e3f752eea53d37b1697ab44ce7
dariohug <d.hugwagner@gmail.com>
2025-03-05 11:09:27 +0100

    cleanup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,44 +1,30 @@
-"use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
+"use client"; 
 
 import { useRouter } from "next/navigation";
 import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { User } from "@/types/user";
-import { Button, Form, Input } from "antd";
-// Optionally, you can import a CSS module or file for additional styling:
-// import styles from "@/styles/page.module.css";
+import { Button, Form, Input, Typography } from "antd";
+import Link from "next/link";
 
-// interface FormFieldProps {
-//   label: string;
-//   value: string;
-// }
+
+const { Text } = Typography;
 
 const Login: React.FC = () => {
   const router = useRouter();
   const apiService = useApi();
   const [form] = Form.useForm();
-  // useLocalStorage hook example use
-  // The hook returns an object with the value and two functions
-  // Simply choose what you need from the hook:
-
-  /*value: token, is commented out because we do not need the token value
-  we need this method to set the value of the token to the one we receive from the POST request to the backend server API
-  clear: clearToken, is commented out because we do not need to clear the token when logging in
-  note that the key we are selecting is "token" and the default value we are setting is an empty string
-  if you want to pick a different token, i.e "usertoken", the line above would look as follows: } = useLocalStorage<string>("usertoken", "");*/
 
   const { set: setToken } = useLocalStorage<string>("token", "");
 
   const handleLogin = async (
     values: { username: string; password: string },
-  ) => {
+    ) => {
     try {
-      // Call the API service and let it handle JSON serialization and error handling
-      // const response = await apiService.post<User>("/users", values);
+
       const response = await apiService.post<User>("/login", values);
       console.log("Login Response:", response);
 
-      // Use the useLocalStorage hook that returned a setter function (setToken in line 41) to store the token if available
       if (response.token) {
         setToken(response.token);
         localStorage.setItem("token", response.token);
@@ -48,7 +34,6 @@ const Login: React.FC = () => {
         localStorage.setItem("userId", response.id);
       } else alert("No userId set in localStorage");
 
-      // Navigate to the user overview
       router.push("/users");
     } catch (error) {
       if (error instanceof Error) {
@@ -88,7 +73,18 @@ const Login: React.FC = () => {
             Login
           </Button>
         </Form.Item>
+
+      <Form.Item style={{ width: "100%", textAlign: "center" }}>
+        <Text>
+          Don't have an account?{" "}
+          <Link href="/register" style={{ color: "#1890ff" }}>
+            Register here
+          </Link>
+        </Text>
+      </Form.Item>
       </Form>
+
+
     </div>
   );
 };
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,13 +1,11 @@
-"use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
+"use client"; 
 
-import { useRouter } from "next/navigation"; // use NextJS router for navigation
+import { useRouter } from "next/navigation";
 import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { User } from "@/types/user";
 import { Button, Form, Input } from "antd";
-// import { handleWebpackExternalForEdgeRuntime } from "next/dist/build/webpack/plugins/middleware-plugin";
-// Optionally, you can import a CSS module or file for additional styling:
-// import styles from "@/styles/page.module.css";
+
 
 interface FormFieldProps {
   username: string;
@@ -19,15 +17,8 @@ const Register: React.FC = () => {
   const router = useRouter();
   const apiService = useApi();
   const [form] = Form.useForm();
-  // useLocalStorage hook example use
-  // The hook returns an object with the value and two functions
-  // Simply choose what you need from the hook:
-  const {
-    // value: token, // is commented out because we do not need the token value
-    set: setToken, // we need this method to set the value of the token to the one we receive from the POST request to the backend server API
-    // clear: clearToken, // is commented out because we do not need to clear the token when logging in
-  } = useLocalStorage<string>("token", ""); // note that the key we are selecting is "token" and the default value we are setting is an empty string
-  // if you want to pick a different token, i.e "usertoken", the line above would look as follows: } = useLocalStorage<string>("usertoken", "");
+
+  const { set: setToken, } = useLocalStorage<string>("token", "");
 
   const handleRegister = async (values: FormFieldProps) => {
     try {
@@ -35,33 +26,37 @@ const Register: React.FC = () => {
 
       console.log(values);
 
-      if (response.token) setToken(response.token);
+      if (response.token) { setToken(response.token); }
 
       handleLogin(values);
+
     } catch (error) {
+
       if (error instanceof Error) {
         alert(`Something went wrong during the login:\n${error.message}`);
-      } else console.error("An unknown error occurred during login.");
+      } else {
+        console.error("An unknown error occurred during login.");
+      }
     }
   };
 
-  const handleLogin = async (
-    values: { username: string; password: string },
-  ) => {
+  const handleLogin = async ( values: { username: string; password: string }, ) => {
     try {
-      // Call the API service and let it handle JSON serialization and error handling
-      // const response = await apiService.post<User>("/users", values);
-      const response = await apiService.post<User>("/login", values);
-      console.log("Login Response:", response);
 
-      // Use the useLocalStorage hook that returned a setter function (setToken in line 41) to store the token if available
+      const response = await apiService.post<User>("/login", values);
+    //   console.log("Login Response:", response);
+
       if (response.token) {
+
         setToken(response.token);
-        localStorage.setItem("token", response.token);
-      } else alert("No token set in localStorage");
+        // localStorage.setItem("token", response.token);
+
+      } else { alert("No token set in localStorage"); }
 
       if (response.id) {
+
         localStorage.setItem("userId", response.id);
+
       } else alert("No userId set in localStorage");
 
       // Navigate to the user overview
